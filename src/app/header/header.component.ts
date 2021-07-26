6c474db491b8f4a8cf9b158cde58e5b1dbacd709
Trung Kien <kien123456k@gmail.com>
2021-07-26 23:22:49 +0700

    ready to publish

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
